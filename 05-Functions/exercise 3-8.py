aee98651bd30ef44fb9e264f4f1b027a61f77b48
David <dkwynia@gmail.com>
2021-02-09 16:45:50 -0600

    update exercise 3.8 again

--- 05-Functions/exercise 3-8.py
+++ 05-Functions/exercise 3-8.py
@@ -1,18 +1,18 @@
 # Define a easy_money function that accepts no parameters 
 # and always returns the value 100.
 def easy_money():
-    print(100)
+    return 100
 
-easy_money()
+print(easy_money())
 
 
 # Define a best_food_ever function that accepts 
 # no parameters and always returns the string “Sushi”.
 
 def best_food_ever():
-    print("Sushi, (not really)")
+    return "Sushi, (not really)"
 
-best_food_ever()
+print(best_food_ever())
 
 # Define a convert_to_currency function that accepts a single parameter (an integer). 
 # The function should convert the argument to a string, prefix it with a dollar sign, and return the result.
@@ -23,8 +23,8 @@ best_food_ever()
 
 def convert_to_currency(amount):
     amount_str = str(amount)
-    print("$"+amount_str)
+    return "$"+amount_str
 
-convert_to_currency(15)
+print(convert_to_currency(15))
 
-convert_to_currency(8)
+print(convert_to_currency(8))
