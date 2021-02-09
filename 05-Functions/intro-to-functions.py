d4b6b79fcabe8164b23389cb4f3deeb2d22e22f7
David <dkwynia@gmail.com>
2021-02-09 16:09:55 -0600

    update intro to functions

--- 05-Functions/intro-to-functions.py
+++ 05-Functions/intro-to-functions.py
@@ -2,5 +2,7 @@ def output_text():
     #pass #does nothing
     print("Welcome to the program!")
     print("It's nice to meet you")
-    print("Have a wonderful day")
+    print("Have a excellent day")
 
+output_text()
+output_text()
