013d42c9b78dc86ed6d76bf23ee6ed56b71d850b
David <dkwynia@gmail.com>
2021-02-10 16:03:24 -0600

    updates

---
+++ 05-Functions/adder1.py
@@ -0,0 +1,15 @@
+def string_adder(a = '',b = ''):
+    return a + ' ' + b
+
+print(string_adder(""))
+
+def multiplier(a = 1, b = 1, c = 1):
+    return a * b * c
+
+print(multiplier(2,3,3))
+
+
+def best_food_ever():
+    print('Sushi')
+
+best_food_ever()
