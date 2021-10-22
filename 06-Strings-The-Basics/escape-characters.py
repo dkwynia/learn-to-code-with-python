898e62c6e14d10893ccb40ca79c1a423555b9b29
David <dkwynia@gmail.com>
2021-10-22 14:48:45 -0500

    revisited

--- 06-Strings-The-Basics/escape-characters.py
+++ 06-Strings-The-Basics/escape-characters.py
@@ -5,6 +5,7 @@ print("This will \nbegin on a \nnew line")
 #tab
 print("\tOnce upon a time")
 
+#include quotes
 print ('"To be or not to be", said Hamlet')
 #or
 print("\"To be or not to be\", said Hamlet")
@@ -12,6 +13,7 @@ print("\"To be or not to be\", said Hamlet")
 print("to print a backslash: \\")
 
 #raw strings?
+
 
 #file_name = "C:\news\travel" <-- will treat the backslashes as escape codes
 
@@ -22,6 +24,7 @@ some_random_number = 5
 some_obscure_calculation = 25
 some_additional_statistic_fetched_from_somewhere = 10
 final = some_random_number + some_obscure_calculation + some_additional_statistic_fetched_from_somewhere
+print(final)
 
 # or you can
 
