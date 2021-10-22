cb424bdc31100fe363c0941f0aa31520a90d1f2e
David <dkwynia@gmail.com>
2021-10-22 14:36:20 -0500

    cleaning up
    Signed-off-by: David <dkwynia@gmail.com>

--- 06-Strings-The-Basics/the-in-and-not-in-operators-for-inclusion-and-exclusion.py
+++ 06-Strings-The-Basics/the-in-and-not-in-operators-for-inclusion-and-exclusion.py
@@ -9,14 +9,14 @@ print("Steven" in announcement) #prints False
 #doesn't check where the string is, or how many times, just is it in the string
 
 print(" " in announcement) #prints True, there is a space in announcment
+print("," in announcement)
 
-
+print("\n")
 
 print("Boris" not in announcement) #prints False
 print("boris" not in announcement) #prints True
 print("Steven" not in announcement) #prints True
 
 print(" " not in announcement) #prints False, there is a space in announcment
-
-
+print("," not in announcement)
 
