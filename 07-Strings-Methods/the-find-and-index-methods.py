96cba4d723f5b804ac779259776cc65de10015c7
David <dkwynia@gmail.com>
2021-10-30 12:19:11 -0500

    done

---
+++ 07-Strings-Methods/the-find-and-index-methods.py
@@ -0,0 +1,14 @@
+# find always returns an integer, either the index number, or -1
+browser = "Google Chrome"
+print(browser.find("C"))
+print(browser.find("Ch"))
+print(browser.find("o"))
+print(browser.find("Z"))
+print()
+print(browser.find("o"))
+print(browser.find("o",3)) #still returns the index of the character, but it starts looking from 3 (remembering that the first char is 0)
+#in only tells that a substring exists, .find tells you where it is
+
+browser = "Google Chrome"
+print(browser.index("C"))
+print(browser.index("Z")) #find returns -1, index crashes out with a ValueError
